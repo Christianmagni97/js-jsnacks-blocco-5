f5371b9a87c9f28e45d8b45f13a936301c40182c
Christianmagni97 <christianmagni28@gmail.com>
2024-05-13 12:58:40 +0200

    6 nomi

---
+++ script.js
@@ -0,0 +1,7 @@
+const userName = prompt('enter a number');
+const userName2 = prompt('enter a number');
+const userName3 = prompt('enter a number');
+const userName4 = prompt('enter a number');
+const userName5 = prompt('enter a number');
+const userName6 = prompt('enter a number');
+console. log(userName);
